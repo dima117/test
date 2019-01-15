108f61dff5f0078c1b7439e1e813d7c4b388ddac
Dmitry Andriyanov <dima117a@yandex-team.ru>
2019-01-15 21:28:49 +0300

    meta

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,20 @@ class App extends Component {
     return (
       <div className="App">
         <Helmet>
-          <meta property="og:type" content="website" />
-          <meta property="twitter:site" content="@insiru" />
-          <meta property="og:url" content="https://www.insi.ru/contacts/russia/moscow/" />
-          <meta property="twitter:url" content="https://www.insi.ru/contacts/russia/moscow/" />
-          <meta property="og:site_name" content="Холдинг ИНСИ" />
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="og:image" content="https://www.insi.ru/html/i/logo.png" />
-          <meta property="twitter:image:src" content="https://www.insi.ru/html/i/logo.png" />
-          <meta property="og:title" content="Контакты ИНСИ Москва"/>
-          <meta property="twitter:title" content="Контакты ИНСИ Москва" />
-          <meta property='og:description' content='Контакты холдинга ИНСИ в Москве.' />
-          <meta property='twitter:description' content='Контакты холдинга ИНСИ в Москве.' />
+          <meta property="og:title" content="Мэрилин Монро" />
+          <meta
+            property="og:description"
+            content="Американская киноактриса и певица"
+          />
+          <meta
+            property="og:image"
+            content="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Marilyn_Monroe_-_publicity.JPG/210px-Marilyn_Monroe_-_publicity.JPG"
+          />
+          <meta property="og:type" content="profile" />
+          <meta
+            property="og:url"
+            content="https://ru.wikipedia.org/wiki/Мэрилин_Монро"
+          />
         </Helmet>
 
         <header className="App-header">
